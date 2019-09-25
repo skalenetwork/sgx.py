1120f91d5ad3fb97591879cc6a79158e038122dd
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-09-25 15:09:41 +0300

    SKALE-1562 Update sgx.py with new vrs format

--- connector.py
+++ connector.py
@@ -4,21 +4,37 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-def main(transactionHash):
+def ecdsa_sign(keyName, transactionHash):
     url = os.environ['SERVER']
     headers = {'content-type': 'application/json'}
 
-    payload = {
+    call_data = {
         "method": "ecdsaSignMessageHash",
-        "params": {'keyName':"test_key", 'messageHash':transactionHash},
+        "params": {'base':10, 'keyName': keyName, 'messageHash':transactionHash},
         "jsonrpc": "2.0",
         "id": 0,
     }
     response = requests.post(
-        url, data=json.dumps(payload), headers=headers).json()
+        url, data=json.dumps(call_data), headers=headers).json()
     signature = response['result']
+    print(signature)
     return(signature['signature_v'], signature['signature_r'], signature['signature_s'])
 
 
+def generate_key(keyName):
+    url = os.environ['SERVER']
+    headers = {'content-type': 'application/json'}
+
+    call_data = {
+        "method": "generateECDSAKey",
+        "params": {'keyName': keyName},
+        "jsonrpc": "2.0",
+        "id": 0,
+    }
+    response = requests.post(
+        url, data=json.dumps(call_data), headers=headers).json()
+    return response
+
+
 if __name__ == "__main__":
-    print(main(hash))
+    print(generate_key('key1'))
--- sgx.py
+++ sgx.py
@@ -6,20 +6,22 @@ from eth_account.datastructures import AttributeDict
 from eth_utils.curried import keccak
 from cytoolz import dissoc
 import connector
+from web3 import Web3
 
-def sign(transaction_dict, private_key):
+
+def sign(transaction_dict, key_name):
     if not isinstance(transaction_dict, Mapping):
         raise TypeError("transaction_dict must be dict-like, got %r" % transaction_dict)
 
-    account = Account.from_key(private_key)
+    address = get_address_from_key(key_name)
 
     # allow from field, *only* if it matches the private key
     if 'from' in transaction_dict:
-        if transaction_dict['from'] == account.address:
+        if transaction_dict['from'] == address:
             sanitized_transaction = dissoc(transaction_dict, 'from')
         else:
             raise TypeError("from field must match key's %s, but it was %s" % (
-                account.address,
+                address,
                 transaction_dict['from'],
             ))
     else:
@@ -31,7 +33,7 @@ def sign(transaction_dict, private_key):
         r,
         s,
         rlp_encoded,
-    ) = sign_transaction_dict(account._key_obj, sanitized_transaction)
+    ) = sign_transaction_dict(key_name, sanitized_transaction)
 
     transaction_hash = keccak(rlp_encoded)
 
@@ -58,7 +60,7 @@ def sign_transaction_dict(eth_key, transaction_dict):
 
     # sign with private key
     # (v, r, s) = signing.sign_transaction_hash(eth_key, transaction_hash, chain_id)
-    (v, r, s) = sign_transaction_hash(transaction_hash, chain_id)
+    (v, r, s) = sign_transaction_hash(eth_key, transaction_hash, chain_id)
     print(v, r, s)
 
     # serialize transaction with rlp
@@ -67,10 +69,29 @@ def sign_transaction_dict(eth_key, transaction_dict):
     return (v, r, s, encoded_transaction)
 
 
-def sign_transaction_hash(transaction_hash, chain_id):
-    (v_raw, r_raw, s_raw) = connector.main(transaction_hash)
-    print(type(v_raw))
-    v = int(v_raw, 0)
-    r = int(r_raw, 0)
-    s = int(s_raw, 0)
+def sign_transaction_hash(eth_key, transaction_hash, chain_id):
+    hash_in_hex = Web3.toHex(transaction_hash)
+    (v_raw, r_raw, s_raw) = connector.ecdsa_sign(hash_in_hex, eth_key)
+    print('int:',v_raw, r_raw, s_raw)
+    print('hex:',hex(int(v_raw)), hex(int(r_raw)), hex(int(s_raw)))
+    v = signing.to_eth_v(int(v_raw), chain_id)
+    r = int(r_raw)
+    s = int(s_raw)
     return (v, r, s)
+
+
+def public_key_to_address(pk):
+    hash = Web3.sha3(hexstr=str(pk))
+    return Web3.toChecksumAddress(Web3.toHex(hash[-20:]))
+
+
+public_keys = {}
+def get_public_key(key_name):
+    if public_keys.get(key_name) == None:
+        public_keys[key_name] = connector.generate_key(key_name)['result']['PublicKey']
+    return public_keys[key_name]
+
+
+def get_address_from_key(key_name):
+    key = get_public_key(key_name)
+    return public_key_to_address(key)
