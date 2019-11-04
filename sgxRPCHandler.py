4ec6001087839523b6e18bec44e16c05164c545b
Oleh Nikolaiev <oleg@skalelabs.com>
2019-11-04 17:11:06 +0200

    SKALE-1742-add-dkg-functionality

--- sgxRPCHandler.py
+++ sgxRPCHandler.py
@@ -33,6 +33,64 @@ class SgxRPCHandler:
         publicKey = response['result']['PublicKey']
         return publicKey
 
+    def generate_dkg_poly(self, poly_name, t):
+        params = dict()
+        params['polyName'] = poly_name
+        params['t'] = t
+        response = self.__send_request("generateDKGPoly", params)
+
+    def get_verification_vector(self, poly_name, n, t):
+        params = dict()
+        params['polyName'] = poly_name
+        params['n'] = n
+        params['t'] = t
+        response = self.__send_request("getVerificationVector", params)
+        verification_vector = response['Verification Vector']
+        return verification_vector
+
+    def get_secret_key_contribution(self, poly_name, concatinated_public_keys, n, t):
+        params = dict()
+        params['polyName'] = poly_name
+        params['n'] = n
+        params['t'] = t
+        params['publicKeys'] = concatinated_public_keys
+        response = self.__send_request("getSecretShare", params)
+        secret_key_contribution = response['SecretShare']
+        return secret_key_contribution
+
+    def verify_secret_share(self, poly_name, eth_key_name, secret_share, n, t, index):
+        params = dict()
+        params['polyName'] = poly_name
+        params['EthKeyName'] = eth_key_name
+        params['SecretShare'] = secret_share
+        params['n'] = n
+        params['t'] = t
+        params['index'] = index
+        response = self.__send_request("DKGVerification", params)
+        result = response['result']
+        return result == 'true'
+
+    def create_bls_private_key(self, poly_name, bls_key_name, eth_key_name, secret_shares):
+        params = dict()
+        params['polyName'] = poly_name
+        params['BLSKeyName'] = bls_key_name
+        params['EthKeyName'] = eth_key_name
+        params['SecretShare'] = secret_shares
+        params['n'] = n
+        params['t'] = t
+        response = self.__send_request("CreateBLSPrivateKey", params)
+
+    def import_bls_private_key(self, key_share_name, n, t, index, key_share):
+        params = dict()
+        params['keyShareName'] = key_share_name
+        params['n'] = n
+        params['t'] = t
+        params['index'] = index
+        params['key_share'] = key_share
+        response = self.__send_request("ImportBLSKeyShare", params)
+        encrypted_key = response['encryptedKeyShare']
+        return encrypted_key
+
     def __send_request(self, method, params):
         url = self.sgx_endpoint
         headers = {'content-type': 'application/json'}
