ef16604c91e65da25d9f2122c69ffce346914e5e
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-11-11 15:25:09 +0200

    SKALE-1771 Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sgx.py',
+      version='0.1',
+      description='SGX',
+      url='http://github.com/skalenetwork/sgx.py',
+      author='SKALE Labs',
+      author_email='support@skalelabs.com')
