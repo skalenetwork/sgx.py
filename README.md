db41cf790f4539395f1cf055bdf69e4b4f7a5786
Chadwick Strange <cstrangedk@users.noreply.github.com>
2019-11-11 14:57:59 +0200

    Add license

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+[![Discord](https://img.shields.io/discord/534485763354787851.svg)](https://discord.gg/vvUtWJB)
+
 # sgx.py
+
+## License
+
+[![License](https://img.shields.io/github/license/skalenetwork/skale.py.svg)](LICENSE)
+
+Copyright (C) 2019-present SKALE Labs
--- sgx_utils.py
+++ sgx_utils.py
@@ -1,3 +1,22 @@
+#    -*- coding: utf-8 -*-
+#
+#     This file is part of sgx.py 
+#
+#     Copyright (C) 2019 SKALE Labs
+#
+#     sgx.py is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as published
+#     by the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     sgx.py is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with sgx.py.  If not, see <https://www.gnu.org/licenses/>.
+
 from web3 import Web3
 
 
