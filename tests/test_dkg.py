8a5f04a53f8dfb3b706f298c77484f22408a5fa1
Oleh Nikolaiev <oleg@skalelabs.com>
2019-11-22 16:48:23 +0200

    SKALE-1770-fix-test

--- tests/test_dkg.py
+++ tests/test_dkg.py
@@ -3,6 +3,7 @@ import os
 from time import sleep
 from dotenv import load_dotenv
 load_dotenv()
+import random
 
 def convert_g2_point_to_hex(data):
     data_hexed = ""
@@ -22,6 +23,8 @@ sgx = SgxClient(os.environ['SERVER'])
 public_keys = []
 key_name = []
 
+random_dkg_id = random.randint(0, 10**50)
+
 for i in range(n):
     generated_key = sgx.generate_key()
     public_keys.append(generated_key.public_key)
@@ -29,7 +32,7 @@ for i in range(n):
     sleep(1)
 
 for i in range(n):
-    poly_name = "POLY:SCHAIN_ID:" + str(0) + ":NODE_ID:" + str(i) + ":DKG_ID:" + str(0)
+    poly_name = "POLY:SCHAIN_ID:" + str(0) + ":NODE_ID:" + str(i) + ":DKG_ID:" + str(random_dkg_id)
     response = sgx.generate_dkg_poly(poly_name, t)
     if not response:
         raise TypeError("failed generate dkg poly for " + str(i))
@@ -37,7 +40,7 @@ for i in range(n):
 
 verification_vector = []
 for i in range(n):
-    poly_name = "POLY:SCHAIN_ID:" + str(0) + ":NODE_ID:" + str(i) + ":DKG_ID:" + str(0)
+    poly_name = "POLY:SCHAIN_ID:" + str(0) + ":NODE_ID:" + str(i) + ":DKG_ID:" + str(random_dkg_id)
     verification_vector.append(sgx.get_verification_vector(poly_name, n, t))
     sleep(1)
 
@@ -51,7 +54,7 @@ for vv in verification_vector:
 
 secret_key_contribution = []
 for i in range(n):
-    poly_name = "POLY:SCHAIN_ID:" + str(0) + ":NODE_ID:" + str(i) + ":DKG_ID:" + str(0)
+    poly_name = "POLY:SCHAIN_ID:" + str(0) + ":NODE_ID:" + str(i) + ":DKG_ID:" + str(random_dkg_id)
     secret_key_contribution.append(
         sgx.get_secret_key_contribution(poly_name, public_keys, n, t))
     sleep(1)
@@ -66,8 +69,8 @@ for i in range(n):
         sleep(1)
 
 for i in range(n):
-    poly_name = "POLY:SCHAIN_ID:" + str(0) + ":NODE_ID:" + str(i) + ":DKG_ID:" + str(0)
-    bls_key_name = "BLS_KEY:SCHAIN_ID:" + str(0) + ":NODE_ID:" + str(i) + ":DKG_ID:" + str(0)
+    poly_name = "POLY:SCHAIN_ID:" + str(0) + ":NODE_ID:" + str(i) + ":DKG_ID:" + str(random_dkg_id)
+    bls_key_name = "BLS_KEY:SCHAIN_ID:" + str(0) + ":NODE_ID:" + str(i) + ":DKG_ID:" + str(random_dkg_id)
     sgx.create_bls_private_key(
         poly_name,
         bls_key_name,
