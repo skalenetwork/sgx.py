f18f9528acdf61d9b81d049ea078f53734478103
badrogger <ivan@skalelabs.com>
2020-01-06 13:23:25 +0200

    Move generate script to top level package

--- sgx/constants.py
+++ sgx/constants.py
@@ -1,10 +1,4 @@
-import os
-
-PROJECT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
-SCRIPTS_DIR = 'scripts'
-SCRIPTS_DIR_PATH = os.path.join(PROJECT_DIR, SCRIPTS_DIR)
-GENERATE_SCRIPT = 'generate.sh'
-GENERATE_SCRIPT_PATH = os.path.join(SCRIPTS_DIR_PATH, GENERATE_SCRIPT)
+GENERATE_SCRIPT_PATH = 'generate.sh'
 
 DEFAULT_TIMEOUT = 10
 CERT_PROVIDER_PORT = '1027'
