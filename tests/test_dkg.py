5a0e1e04fe66b40a36743d35df6fa2c7cd012ae1
Oleh <oleg@skalelabs.com>
2020-12-15 20:41:06 +0200

    SKALE-3672 fix tests

--- tests/test_dkg.py
+++ tests/test_dkg.py
@@ -154,7 +154,7 @@ def perform_dkg(t, n, with_0x=True, with_complaint=False):
 
             assert ":".join(public_key) == public_keys[i]
 
-            message = ''.join(random.choices(string.ascii_uppercase + string.digits, k=32))
+            message = ''.join(random.choices(string.ascii_lowercase + string.digits, k=32))
 
             signature_share = sgx.bls_sign(bls_key_name, message)
             sig_x, sig_y, hint = signature_share.split(':')
@@ -260,7 +260,7 @@ def test_import():
 
     assert len(response) > 0
 
-    message = ''.join(random.choices(string.ascii_uppercase + string.digits, k=32))
+    message = ''.join(random.choices(string.ascii_lowercase + string.digits, k=32))
 
     signature_share = sgx.bls_sign(bls_key_name, message)
     sig_x, sig_y, hint = signature_share.split(':')
