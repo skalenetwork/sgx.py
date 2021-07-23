85cfb4fa5668a9b26c31c299007de1c0933c6475
badrogger <ivan@skalelabs.com>
2021-07-23 15:55:23 +0300

    Fix eth-account dependency

--- setup.py
+++ setup.py
@@ -28,7 +28,8 @@ setup(
     author='SKALE Labs',
     author_email='support@skalelabs.com',
     install_requires=[
-      "web3==5.13.1"
+      "web3==5.13.1",
+      "eth-account==0.5.5"
     ],
     packages=find_packages(exclude=['tests']),
     python_requires='>=3.7,<4',
