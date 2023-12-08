acdb6738127f25b44ff92aae097cc85406fe57fd
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2023-12-08 18:24:59 +0000

    Add timeout for post

--- sgx/constants.py
+++ sgx/constants.py
@@ -4,6 +4,7 @@ CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 GENERATE_SCRIPT_PATH = os.path.join(CUR_DIR, 'generate.sh')
 
 DEFAULT_TIMEOUT = 10
+SERVER_RESPONSE_TIMEOUT = 60  # seconds
 
 CSR_FILENAME = 'sgx.csr'
 KEY_FILENAME = 'sgx.key'
