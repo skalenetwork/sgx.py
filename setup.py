2816f92528cc33314a2013e660d30887ab489d15
Dmytro <dmitry.tkk@gmail.com>
2025-04-10 17:50:51 +0100

    Update TypedTransaction, add ruff.toml

--- setup.py
+++ setup.py
@@ -5,20 +5,18 @@ from setuptools import (
 
 extras_require = {
     'linter': [
-        "flake8==3.8.3",
+        'flake8==3.8.3',
     ],
     'dev': [
-        "coincurve==13.0.0",
-        "python-dotenv==0.13.0",
-        "twine==3.1.1",
-        "pytest==7.3.1",
-        "mock==4.0.2"
-    ]
+        'coincurve==13.0.0',
+        'python-dotenv==0.13.0',
+        'twine==3.1.1',
+        'pytest==7.3.1',
+        'mock==4.0.2',
+    ],
 }
 
-extras_require['dev'] = (
-    extras_require['linter'] + extras_require['dev']
-)
+extras_require['dev'] = extras_require['linter'] + extras_require['dev']
 
 setup(
     name='sgx.py',
@@ -28,16 +26,14 @@ setup(
     author='SKALE Labs',
     author_email='support@skalelabs.com',
     install_requires=[
-      'web3>=6.20.2,<8.0.0',
-      'pyzmq==25.0.2',
-      'pem==21.2.0',
-      'M2Crypto>=0.40.1,<1.0.0',
-      'urllib3==1.26.0'
+        'web3>=6.20.2,<8.0.0',
+        'pyzmq==25.0.2',
+        'pem==21.2.0',
+        'M2Crypto>=0.40.1,<1.0.0',
+        'urllib3==1.26.0',
     ],
     packages=find_packages(exclude=['tests']),
     python_requires='>=3.7,<4',
     extras_require=extras_require,
-    package_data={
-        'sgx': ['generate.sh']
-    }
+    package_data={'sgx': ['generate.sh']},
 )
--- sgx/sgx.py
+++ sgx/sgx.py
@@ -29,7 +29,7 @@ from sgx.sgx_zmq import SgxZmq
 from sgx.utils import public_key_to_address
 from eth_account._utils import signing
 from eth_account._utils.legacy_transactions import encode_transaction
-from eth_account._utils.typed_transactions import TypedTransaction
+from eth_account.typed_transactions.typed_transaction import TypedTransaction
 from eth_utils.encoding import big_endian_to_int
 from eth_utils.conversions import add_0x_prefix, remove_0x_prefix
 
@@ -67,25 +67,17 @@ class SgxClient:
         key_name, public_key = self.sgx_rpc_server.generate_key()
         public_key = add_0x_prefix(public_key)
         address = public_key_to_address(public_key)
-        return Account(
-            name=key_name,
-            address=address,
-            public_key=public_key
-        )
+        return Account(name=key_name, address=address, public_key=public_key)
 
     def get_account(self, key_name):
         key = self.sgx_rpc_server.get_public_key(key_name)
         key = add_0x_prefix(key)
         address = public_key_to_address(key)
-        return Account(
-            name=key_name,
-            address=address,
-            public_key=key
-        )
+        return Account(name=key_name, address=address, public_key=key)
 
     def sign(self, transaction_dict, key_name):
         if not isinstance(transaction_dict, Mapping):
-            raise TypeError("transaction_dict must be dict-like, got %r" % transaction_dict)
+            raise TypeError('transaction_dict must be dict-like, got %r' % transaction_dict)
 
         address = self.get_account(key_name).address
 
@@ -94,10 +86,13 @@ class SgxClient:
             if transaction_dict['from'] == address:
                 sanitized_transaction = dissoc(transaction_dict, 'from')
             else:
-                raise TypeError("from field must match key's %s, but it was %s" % (
-                    address,
-                    transaction_dict['from'],
-                ))
+                raise TypeError(
+                    "from field must match key's %s, but it was %s"
+                    % (
+                        address,
+                        transaction_dict['from'],
+                    )
+                )
         else:
             sanitized_transaction = transaction_dict
 
@@ -111,31 +106,18 @@ class SgxClient:
 
         transaction_hash = keccak(rlp_encoded)
         return SignedTransaction(
-            rawTransaction=HexBytes(rlp_encoded),
-            hash=HexBytes(transaction_hash),
-            r=r,
-            s=s,
-            v=v
+            rawTransaction=HexBytes(rlp_encoded), hash=HexBytes(transaction_hash), r=r, s=s, v=v
         )
 
     def sign_hash(self, message, key_name, chain_id):
         msg_hash_bytes = HexBytes(message)
         if len(msg_hash_bytes) != 32:
-            raise ValueError("The message hash must be exactly 32-bytes")
+            raise ValueError('The message hash must be exactly 32-bytes')
 
-        (v, r, s) = self._sign_hash(
-            key_name,
-            msg_hash_bytes,
-            chain_id,
-            eip_1559_identifier=False
-        )
+        (v, r, s) = self._sign_hash(key_name, msg_hash_bytes, chain_id, eip_1559_identifier=False)
         signature_bytes = signing.to_bytes32(r) + signing.to_bytes32(s) + signing.to_bytes(v)
         return SignedMessage(
-            messageHash=msg_hash_bytes,
-            r=r,
-            s=s,
-            v=v,
-            signature=HexBytes(signature_bytes)
+            messageHash=msg_hash_bytes, r=r, s=s, v=v, signature=HexBytes(signature_bytes)
         )
 
     def generate_dkg_poly(self, poly_name):
@@ -164,39 +146,23 @@ class SgxClient:
 
     def verify_secret_share(self, public_shares, eth_key_name, secret_share, index):
         return self.sgx_rpc_server.verify_secret_share(
-            public_shares,
-            eth_key_name,
-            secret_share,
-            self.n,
-            self.t,
-            index)
+            public_shares, eth_key_name, secret_share, self.n, self.t, index
+        )
 
     def verify_secret_share_v2(self, public_shares, eth_key_name, secret_share, index):
         return self.sgx_rpc_server.verify_secret_share_v2(
-            public_shares,
-            eth_key_name,
-            secret_share,
-            self.n,
-            self.t,
-            index)
+            public_shares, eth_key_name, secret_share, self.n, self.t, index
+        )
 
     def create_bls_private_key(self, poly_name, bls_key_name, eth_key_name, secret_shares):
         return self.sgx_rpc_server.create_bls_private_key(
-            poly_name,
-            bls_key_name,
-            eth_key_name,
-            secret_shares,
-            self.n,
-            self.t)
+            poly_name, bls_key_name, eth_key_name, secret_shares, self.n, self.t
+        )
 
     def create_bls_private_key_v2(self, poly_name, bls_key_name, eth_key_name, secret_shares):
         return self.sgx_rpc_server.create_bls_private_key_v2(
-            poly_name,
-            bls_key_name,
-            eth_key_name,
-            secret_shares,
-            self.n,
-            self.t)
+            poly_name, bls_key_name, eth_key_name, secret_shares, self.n, self.t
+        )
 
     def get_bls_public_key(self, bls_key_name):
         return self.sgx_rpc_server.get_bls_public_key(bls_key_name)
@@ -206,9 +172,7 @@ class SgxClient:
             poly_name, self.n, self.t, idx
         )
         return ComplaintResponse(
-            share=share,
-            dh_key=dh_key,
-            verification_vector_mult=verification_vector_mult
+            share=share, dh_key=dh_key, verification_vector_mult=verification_vector_mult
         )
 
     def mult_g2(self, to_mult):
@@ -236,9 +200,7 @@ class SgxClient:
 
     def _sign_transaction_dict(self, eth_key, transaction_dict):
         # generate RLP-serializable transaction, with defaults filled
-        unsigned_tx = signing.serializable_unsigned_transaction_from_dict(
-            transaction_dict
-        )
+        unsigned_tx = signing.serializable_unsigned_transaction_from_dict(transaction_dict)
 
         transaction_hash = unsigned_tx.hash()
 
@@ -249,27 +211,14 @@ class SgxClient:
         if isinstance(unsigned_tx, TypedTransaction):
             eip_1559_identifier = True
 
-        (v, r, s) = self._sign_hash(
-            eth_key,
-            transaction_hash,
-            chain_id,
-            eip_1559_identifier
-        )
+        (v, r, s) = self._sign_hash(eth_key, transaction_hash, chain_id, eip_1559_identifier)
 
         # serialize transaction with rlp
-        encoded_tx = encode_transaction(
-            unsigned_tx,
-            vrs=(v, r, s))
+        encoded_tx = encode_transaction(unsigned_tx, vrs=(v, r, s))
 
         return v, r, s, encoded_tx
 
-    def _sign_hash(
-        self,
-        eth_key,
-        transaction_hash,
-        chain_id,
-        eip_1559_identifier=False
-    ):
+    def _sign_hash(self, eth_key, transaction_hash, chain_id, eip_1559_identifier=False):
         hash_in_hex = hex(big_endian_to_int(transaction_hash))
         (v_raw, r_raw, s_raw) = self.sgx_rpc_server.ecdsa_sign(eth_key, hash_in_hex)
         if eip_1559_identifier:
