e09f5cfd1f03aa99728024ee03aa48e9ec7bc518
Dmytro <dmitry.tkk@gmail.com>
2025-04-16 13:06:48 +0100

    Upgrade pyzmq to 26.4.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extras_require['dev'] = extras_require['linter'] + extras_require['dev']
 
 setup(
     name='sgx.py',
-    version='0.9',
+    version='0.10',
     description='SGX',
     url='http://github.com/skalenetwork/sgx.py',
     author='SKALE Labs',
